712236d49c9d9cab2d5fa94c7a047a73a0de372c
LENOVO <d.s.arum28@gmail.com>
2022-06-26 11:55:59 +0700

    Recyclerview App

--- app/src/main/java/com/dewisekararum/recyclerview/MainActivity.kt
+++ app/src/main/java/com/dewisekararum/recyclerview/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.dewisekararum.recyclerview
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.dewisekararum.recyclerview.adapter.AdapterTeamBola
 import com.dewisekararum.recyclerview.databinding.ActivityMainBinding
 import com.dewisekararum.recyclerview.model.Pemain
@@ -15,14 +20,37 @@ class MainActivity : AppCompatActivity() {
 
         val listPemain = ArrayList<Pemain>()
         listPemain.add(Pemain("Thibaut Courtois",R.drawable.courtois,"Penjaga Gawang", "2.00 m", "Bree (Belgia)", "11  Mei 1992"))
-        listPemain.add(Pemain("Karim Benzema",R.drawable.courtois,"Penyerang", "1.85 m", "Lyon (Perancis)", "19 Desember 1987"))
-        listPemain.add(Pemain("Marcelo Vieira da Silva",R.drawable.courtois,"Belakang", "1.74 m", "Rio de Janeiro (Brasil)", "12 Mei 1988"))
-        listPemain.add(Pemain("Sergio Ramos García",R.drawable.courtois,"Belakang", "1.84 m", "Camas (Sevilla)", "30 Maret 1986"))
-        listPemain.add(Pemain("Zinedine Yazid Zidane",R.drawable.courtois,"Pelatih", "1.85 m", "Marseille (Prancis)", "23 Juni 1972"))
+        listPemain.add(Pemain("Karim Benzema",R.drawable.benzema,"Penyerang", "1.85 m", "Lyon (Perancis)", "19 Desember 1987"))
+        listPemain.add(Pemain("Marcelo Vieira da Silva",R.drawable.marcello,"Belakang", "1.74 m", "Rio de Janeiro (Brasil)", "12 Mei 1988"))
+        listPemain.add(Pemain("Sergio Ramos García",R.drawable.ramos,"Belakang", "1.84 m", "Camas (Sevilla)", "30 Maret 1986"))
+        listPemain.add(Pemain("Zinedine Yazid Zidane",R.drawable.zidan,"Pelatih", "1.85 m", "Marseille (Prancis)", "23 Juni 1972"))
 
         binding.list.adapter = AdapterTeamBola(this,listPemain,object : AdapterTeamBola.OnClickListener{
             override fun detailData(item: Pemain?) {
-                TODO("Not yet implemented")
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detail_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.txtNamaPemain)
+                    val posisi = this.findViewById<TextView>(R.id.txtPosisi)
+                    val tinggi = this.findViewById<TextView>(R.id.txtTinggi)
+                    val tempatlahir= this.findViewById<TextView>(R.id.txtTempatLahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.txtTanggalLahir)
+                    val btn = this.findViewById<Button>(R.id.btnClose)
+
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text = "${item?.nama}"
+                    posisi.text = "${item?.posisi}"
+                    tinggi.text = "${item?.tinggi}"
+                    tempatlahir.text = "${item?.tempatlahir}"
+                    tgllahir.text = "${item?.tgllahir}"
+
+                    btn.setOnClickListener {
+                        this.dismiss()
+                    }
+                }.show()
             }
 
         })
